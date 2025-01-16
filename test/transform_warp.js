540ebe965e08cdfeb339e93953b03b0e1c5894c4
William Berg <william.berg08@gmail.com>
2025-01-16 20:51:18 +0000

    add transform warp test baseline

--- test/transform_warp.js
+++ test/transform_warp.js
@@ -16,62 +16,78 @@ const div2 = document.createElement('div')
 
 slider1.classList.add('slider');
 slider1.type = 'range';
-slider1.min = 0
+slider1.min = -10
 slider1.max = 10
 slider1.value = 5
 slider1.step = 0.1
 slider1.style.left = `120px`;
 slider1.style.top = `465px`;
-slider1.style.width = `200px`
+slider1.style.width = '500px'
 
 slider2.classList.add('slider');
 slider2.type = 'range';
-slider2.min = 0
+slider2.min = -10
 slider2.max = 10
 slider2.value = 5
-slider2.step = 1
+slider2.step = 0.1
 slider2.style.left = `120px`;
 slider2.style.top = `515px`;
-slider2.style.width = `200px`
+slider2.style.width = '500px'
 
 slider3.classList.add('slider');
 slider3.type = 'range';
-slider3.min = 0
+slider3.min = -10
 slider3.max = 10
-slider3.step = 1
+slider3.step = 0.1
 slider3.style.left = `120px`;
 slider3.style.top = `565px`;
-slider3.style.width = `200px`
+slider3.style.width = '500px'
 
 slider4.classList.add('slider');
 slider4.type = 'range';
-slider4.min = 0
+slider4.min = -10
 slider4.max = 10
 slider4.value = 5
-slider4.step = 1
+slider4.step = 0.1
 slider4.style.left = `120px`;
 slider4.style.top = `615px`;
-slider4.style.width = `200px`
+slider4.style.width = '500px'
 
 slider5.classList.add('slider');
 slider5.type = 'range';
-slider5.min = 0
+slider5.min = -10
 slider5.max = 10
 slider5.value = 5
-slider5.step = 1
+slider5.step = 0.1
 slider5.style.left = `120px`;
 slider5.style.top = `665px`;
-slider5.style.width = `200px`
+slider5.style.width = '500px'
 
 slider6.classList.add('slider');
 slider6.type = 'range';
-slider6.min = 0
+slider6.min = -10
 slider6.max = 10
 slider6.value = 5
-slider6.step = 1
+slider6.step = 0.1
 slider6.style.left = `120px`;
 slider6.style.top = `715px`;
-slider6.style.width = `200px`
+slider6.style.width = '500px'
+
+const resetTransformButton = document.createElement('button');
+resetTransformButton.textContent = 'Reset parameters'
+resetTransformButton.addEventListener('click', () => {
+  slider1.value = -1
+  slider2.value = 6
+  slider3.value = 9
+  slider4.value = 0
+  slider5.value = 0
+  slider6.value = 0
+});
+resetTransformButton.classList.add('slider')
+resetTransformButton.style.left = `120px`;
+resetTransformButton.style.top = `765px`;
+resetTransformButton.style.width = '500px'
+
 
 document.body.appendChild(slider1)
 // document.body.appendChild(div1)
@@ -81,6 +97,7 @@ document.body.appendChild(slider3)
 document.body.appendChild(slider4)
 document.body.appendChild(slider5)
 document.body.appendChild(slider6)
+document.body.appendChild(resetTransformButton)
 
 const canvas = document.getElementById('game')
 const ctx = canvas.getContext('2d')
@@ -134,21 +151,21 @@ function gameLoop() {
   ctx.save()
   ctx.transform(slider1.value / 10,slider2.value / 10,slider3.value / 10,slider4.value / 10,slider5.value / 10,slider6.value / 10);
 
-  ctx.fillStyle = 'hsl(' + Math.sin(tick / 1000) * 100  + ' 100% 50%)'
+  ctx.fillStyle = 'hsl(' + Math.sin(tick / 500) * 100  + ' 100% 50%)'
   ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
-  const checkColour1Hue = checkColour1SandHue + Math.sin(tick/500) * (checkColour1WaterHue - checkColour1SandHue)
-  const checkColour1Sat = checkColour1SandSat + Math.sin(tick/500) * (checkColour1WaterSat - checkColour1SandSat)
-  const checkColour1Lum = checkColour1SandLum + Math.sin(tick/500) * (checkColour1WaterLum - checkColour1SandLum)
-  const checkColour2Hue = checkColour2SandHue + Math.sin(tick/500) * (checkColour2WaterHue - checkColour2SandHue)
-  const checkColour2Sat = checkColour2SandSat + Math.sin(tick/500) * (checkColour2WaterSat - checkColour2SandSat)
-  const checkColour2Lum = checkColour2SandLum + Math.sin(tick/500) * (checkColour2WaterLum - checkColour2SandLum)
+  const checkColour1Hue = checkColour1SandHue + Math.sin(tick/50) * (checkColour1WaterHue - checkColour1SandHue)
+  const checkColour1Sat = checkColour1SandSat + Math.sin(tick/50) * (checkColour1WaterSat - checkColour1SandSat)
+  const checkColour1Lum = checkColour1SandLum + Math.sin(tick/50) * (checkColour1WaterLum - checkColour1SandLum)
+  const checkColour2Hue = checkColour2SandHue + Math.sin(tick/50) * (checkColour2WaterHue - checkColour2SandHue)
+  const checkColour2Sat = checkColour2SandSat + Math.sin(tick/50) * (checkColour2WaterSat - checkColour2SandSat)
+  const checkColour2Lum = checkColour2SandLum + Math.sin(tick/50) * (checkColour2WaterLum - checkColour2SandLum)
 
   ctx.lineWidth = 1
   // for (let row = -2; row - 2 < canvasHeight / cellSize; row++) {
   //   for (let col = -2; col - 2 < canvasWidth / cellSize; col++) {
-  for (let row = -2; row - 20 < canvasHeight / cellSize; row++) {
-    for (let col = -2; col - 20 < canvasWidth / cellSize; col++) {
+  for (let row = -2; row + 10 < canvasHeight / cellSize; row++) {
+    for (let col = -2; col + 10 < canvasWidth / cellSize; col++) {
       const checkColourAlpFill = (row * 3 / (canvasHeight * 3 / cellSize)) + 0.2
       const checkColourAlpStroke = (row * 3 / (canvasHeight * 3 / cellSize)) - 0.2
 
@@ -192,17 +209,17 @@ function gameLoop() {
   ]
   const crossSize = 1
 
-  const crossColour1Hue = crossColour1SandHue + Math.sin(tick/500) * (crossColour1WaterHue - crossColour1SandHue)
-  const crossColour1Sat = crossColour1SandSat + Math.sin(tick/500) * (crossColour1WaterSat - crossColour1SandSat)
-  const crossColour1Lum = crossColour1SandLum + Math.sin(tick/500) * (crossColour1WaterLum - crossColour1SandLum)
-  const crossColour2Hue = crossColour2SandHue + Math.sin(tick/500) * (crossColour2WaterHue - crossColour2SandHue)
-  const crossColour2Sat = crossColour2SandSat + Math.sin(tick/500) * (crossColour2WaterSat - crossColour2SandSat)
-  const crossColour2Lum = crossColour2SandLum + Math.sin(tick/500) * (crossColour2WaterLum - crossColour2SandLum)
+  const crossColour1Hue = crossColour1SandHue + Math.sin(tick/50) * (crossColour1WaterHue - crossColour1SandHue)
+  const crossColour1Sat = crossColour1SandSat + Math.sin(tick/50) * (crossColour1WaterSat - crossColour1SandSat)
+  const crossColour1Lum = crossColour1SandLum + Math.sin(tick/50) * (crossColour1WaterLum - crossColour1SandLum)
+  const crossColour2Hue = crossColour2SandHue + Math.sin(tick/50) * (crossColour2WaterHue - crossColour2SandHue)
+  const crossColour2Sat = crossColour2SandSat + Math.sin(tick/50) * (crossColour2WaterSat - crossColour2SandSat)
+  const crossColour2Lum = crossColour2SandLum + Math.sin(tick/50) * (crossColour2WaterLum - crossColour2SandLum)
 
   // for (let row = -8; row < canvasHeight / cellSize; row++) {
   //   for (let col = 0; col - 8 < canvasWidth / cellSize; col++) {
-  for (let row = -8; row - 20 < canvasHeight / cellSize; row++) {
-    for (let col = 0; col - 28 < canvasWidth / cellSize; col++) {
+  for (let row = -8; row + 10 < canvasHeight / cellSize; row++) {
+    for (let col = 0; col + 10 < canvasWidth / cellSize; col++) {
       const crossColorAlp = (row * 3 / (canvasHeight * 3 / cellSize) ) + 0.2
 
       const crossColour1 = 'hsla(' + crossColour1Hue + ' ' + crossColour1Sat + ' ' + crossColour1Lum + ' / ' + crossColorAlp + ')'
